42bd69d7bb63c758ea3a884dc75311663f4ad471
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-05-18 19:01:36 +0530

    Added: Employee data

---
+++ src/main/java/com/bridgelabz/jdbc/EmployeePayrollData.java
@@ -0,0 +1,44 @@
+package com.bridgelabz.jdbc;
+
+import java.util.Date;
+/*
+ *Author: Prasad
+ */
+public class EmployeePayrollData {
+    private int id;
+    private String name;
+    private double salary;
+    private Date date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String eName) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
